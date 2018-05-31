4b6ebeee26f1fd29ef40be81777a44f95e6fe641
= <falecom@vitorleonel.com>
2018-05-31 19:56:58 -0300

    iniciando front-end

---
+++ eans.vitorleonel.com/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <search />
+  </div>
+</template>
+
+<script>
+  import Search from './components/Search'
+
+  export default {
+    components: {
+      Search
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import url('//fonts.googleapis.com/css?family=Poppins:300,400,700');
+  @import url('//use.fontawesome.com/releases/v5.0.13/css/all.css');
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Poppins', sans-serif;
+    font-size: 100%;
+    background-color: #ffd700;
+    background-image: url('/assets/background.jpg');
+    background-position: center bottom;
+    margin: 0;
+  }
+
+  .container {
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
+
---
+++ eans.vitorleonel.com/src/components/Search.vue
@@ -0,0 +1,72 @@
+<template>
+  <form class="search" @submit.prevent="submitForm">
+    <input type="text" placeholder="Que produto você esta procurando hoje?">
+
+    <button>
+      <i class="fas fa-search"></i>
+    </button>
+  </form>
+</template>
+
+<script>
+  export default {
+    methods: {
+      submitForm() {}
+    }
+  }
+</script>
+
+
+<style lang="scss">
+  .search {
+    width: 30vw;
+    padding: 0.5rem;
+    background-color: white;
+    box-shadow: 0 1px 15px 1px rgba(69, 65, 78, 0.2);
+    border-radius: 6px;
+    position: relative;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    &:hover {
+      box-shadow: 0 1px 20px 1px rgba(69, 65, 78, 0.4);
+    }
+
+    input,
+    button {
+      font-family: 'Poppins', sans-serif;
+      font-size: 1.25rem;
+      font-weight: 400;
+      padding: 0.5rem 1.25rem;
+      outline: none;
+    }
+
+    input {
+      width: 100%;
+      padding-left: 0.35rem;
+      border-color: transparent;
+      color: #636e72;
+
+      &::placeholder {
+        color: lighten(#636e72, 40);
+      }
+    }
+
+    button {
+      border: 1px solid darken(#2d3436, 10);
+      background-color: lighten(#2d3436, 10);
+      color: white;
+      border-radius: 6px;
+
+      &:hover,
+      &:focus {
+        background-color: #2d3436;
+        cursor: pointer;
+      }
+    }
+  }
+</style>
+
